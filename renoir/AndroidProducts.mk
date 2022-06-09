fe48a89d91ec4ba12dc83fbaf214d90b9e5f1fed
Mirageu <33937098+Mirageu@users.noreply.github.com>
2022-06-09 13:22:46 +0200

    Add files via upload

---
+++ renoir/AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/corvus_renoir.mk
+
+COMMON_LUNCH_CHOICES := \
+    corvus_renoir-user \
+    corvus_renoir-userdebug \
+    corvus_renoir-eng
